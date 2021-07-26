a122f3008386878d40ea72ecd9e709168a940388
TobiasWeber <weber68@hm.edu>
2021-07-26 19:01:24 +0200

    zweite

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css'
 
 import Amplify from '@aws-amplify/core';
 import awsmobile from './aws-exports';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listChallanges } from './graphql/queries';
 
 import { withAuthenticator } from 'aws-amplify-react';
 
 Amplify.configure(awsmobile);
 
-function App() {
-  return (
-    <h1>
-      Hello World!
-    </h1>
-  );
+class App extends Component {
+  state = { challanges: [] }
+  async componentDidMount() {
+    const data = await API.graphql(graphqlOperation(listChallanges))
+    this.setState({
+      challanges: data.data.listChallanges.items
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.challanges.map((challange, index) => (<p key={index}>
+          {challange.content}   {challange.id}
+          </p>))}
+      </div >
+    );
+  }
 }
 
 export default withAuthenticator(App, {
