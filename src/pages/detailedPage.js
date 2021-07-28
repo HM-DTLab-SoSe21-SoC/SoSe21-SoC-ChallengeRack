b69d42527e0b0375fe4ba23b97fc0dc483f85e20
TobiasWeber <weber68@hm.edu>
2021-07-28 09:26:03 +0200

    1.3

--- src/pages/detailedPage.js
+++ src/pages/detailedPage.js
@@ -4,12 +4,25 @@ import { API } from 'aws-amplify';
 import { listChallanges } from '../graphql/queries';
 
 import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
   },
-});
+}));
+
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
 
 export default function DataGridDemo() {
   const classes = useStyles();
@@ -24,7 +37,30 @@ export default function DataGridDemo() {
     setNotes(apiData.data.listChallanges.items);
   }
   return (
-    <div className="App">
+    <div className={classes.root}>
+      <List component="nav" aria-label="main mailbox folders">
+        <ListItem button>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Inbox" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <DraftsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Drafts" />
+        </ListItem>
+      </List>
+      <Divider />
+      <List component="nav" aria-label="secondary mailbox folders">
+        <ListItem button>
+          <ListItemText primary="Trash" />
+        </ListItem>
+        <ListItemLink href="#simple-list">
+          <ListItemText primary="Spam" />
+        </ListItemLink>
+      </List>
       <p>Hallo</p>
       <Link to="/challangeOverview">
         <button variant="outlined">
