6c96e960474bbd1a369ea3b7a83e9c86493873b8
TobiasWeber <weber68@hm.edu>
2021-07-28 10:02:24 +0200

    1.5

--- src/pages/challangeOverview.js
+++ src/pages/challangeOverview.js
@@ -12,6 +12,8 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
+
+
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
